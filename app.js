9951960b8a89d8c216b26669f9027e77edc43c33
tkrain23 <tk.rain09@gmail.com>
2018-09-01 17:55:47 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+let reviews = [
+    { title: "Great Review" },
+    { title: "Next Review" }
+]
+
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
@@ -11,6 +16,10 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// app.get('/', (req, res) => {
+//   res.render('home', { msg: 'Hello World!' });
+// })
+
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
+    res.render('reviews-index', { reviews: reviews });
 })
