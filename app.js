e88078f4dfd95e49f7386ed5d5a9fce6f455f646
tkrain23 <tk.rain09@gmail.com>
2018-09-04 11:17:53 -0700

    User can see single reviews

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true }
 const Review = mongoose.model('Review', {
   title: String,
   description: String,
-  movieTitle: String
+  movieTitle: String,
+  rating: Number
 });
 
 const bodyParser = require('body-parser');
@@ -49,8 +50,18 @@ app.get('/reviews/new', (req, res) => {
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
+})
+
+// SHOW
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
   }).catch((err) => {
     console.log(err.message);
   })
