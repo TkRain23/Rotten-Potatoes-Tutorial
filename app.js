d4d05d6a5e315263f746f6a5ebd864e8984e4c72
tkrain23 <tk.rain09@gmail.com>
2018-09-06 13:31:38 -0700

    fixed deployment

--- app.js
+++ app.js
@@ -100,6 +100,6 @@ app.delete('/reviews/:id', function (req, res) {
 var routes = require('./controllers/reviews');
 routes(app, Review);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('App listening on port 3000!')
 })
