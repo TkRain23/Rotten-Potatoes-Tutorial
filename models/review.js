1f97b1dd88cb926622822778caf752724784f096
tkrain23 <tk.rain09@gmail.com>
2018-09-11 01:29:37 -0700

    Fixed error where ratings were not displaying

--- models/review.js
+++ models/review.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose')
 const Review = mongoose.model('Review', {
   title: String,
   description: String,
-  movieTitle: String
+  movieTitle: String,
+  rating: Number
 });
 
 module.exports = Review
