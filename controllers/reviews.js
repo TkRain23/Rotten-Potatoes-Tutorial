ceadadb8c2e8578e77a1c8d9b05fecef4ed25c72
tkrain23 <tk.rain09@gmail.com>
2018-10-15 18:50:34 -0700

    fixed

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,15 +1,26 @@
 //reviews.js
 
-module.exports = function (app, Review) {
+module.exports = function(app, Review) {
 
-  app.get('/', (req, res) => {
-    Review.find()
-      .then(reviews => {
-        res.render('reviews-index', {reviews: reviews});
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  });
+    app.get('/', (req, res) => {
+        Review.find()
+            .then(reviews => {
+                res.render('reviews-index', {
+                    reviews: reviews
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    });
 
+    // DELETE
+    app.delete('/reviews/:id', function(req, res) {
+        console.log("DELETE review")
+        Review.findByIdAndRemove(req.params.id).then((review) => {
+            res.redirect('/');
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    })
 }
